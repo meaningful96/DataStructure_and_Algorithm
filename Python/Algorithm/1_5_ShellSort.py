9b40276d00f04c05aa2b2f69daeff7bacebddcd0
Youm <111734605+meaningful96@users.noreply.github.com>
2022-12-02 17:51:36 +0900

    Add files via upload

---
+++ Python/Algorithm/1_5_ShellSort.py
@@ -0,0 +1,30 @@
+# Deep Learning for AI engineer
+"""
+Created on Youminkk
+
+
+Nil sine magno vitae labore dedit mortalibus
+"""
+
+def ShellSort(list):
+    distance = len(list)//2
+    while distance > 0:
+        for i in range(distance, len(list)):
+            temp = list[i]
+            j = i
+            # 하위 리스트 안에 든 요소들을 정렬합니다.
+            while j >= distance and list[j - distance] > temp:
+                j = j - distance
+            list[j] = temp
+        #다음 패스를 위해 거리를 반으로 줄입니다.
+        distance = distance//2
+    return list
+    
+if __name__ == "__main__":
+    listIn = [26,17,20,11,23,21,13,18,24,14,12,22,16,15,19,25]
+    print(listIn)
+    Output = ShellSort(listIn)
+    print(Output)
+    
+    
+    
